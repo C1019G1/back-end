6675cd4ea4edd7e425dbb6df2a4c57cde91b0a0e
Shinya9082 <anhtuandtu1993@gmail.com>
2020-03-03 17:17:18 +0700

    Update Entity

--- module_4/dao/src/main/java/com/codegym/dao/entity/User.java
+++ module_4/dao/src/main/java/com/codegym/dao/entity/User.java
@@ -3,34 +3,72 @@ package com.codegym.dao.entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
     private Long id;
 
-    @Column(name = "user_name")
+    @Column()
     private String userName;
 
-    @Column(name = "user_password")
+    @Column()
     private String password;
 
-    @Column(name = "user_profile_id")
-    private Long userInfoId;
-
     @ManyToOne
-    @JoinTable(name = "user_role")
-    @JoinColumn(name = "user_role_id")
+    @JoinColumn(name = "role_id")
     private Role role;
 
-    @OneToOne(fetch = FetchType.LAZY,
-            cascade =  CascadeType.ALL,
-            mappedBy = "user")
+    @OneToOne
+    @JoinColumn(name = "user_profile_id")
     private UserProfile userProfile;
 
     public User() {
     }
 
+    public User(String userName, String password, Role role, UserProfile userProfile) {
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+        this.userProfile = userProfile;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public UserProfile getUserProfile() {
+        return userProfile;
+    }
+
+    public void setUserProfile(UserProfile userProfile) {
+        this.userProfile = userProfile;
+    }
 }
--- module_4/service/src/main/java/com/codegym/service/UserService.java
+++ module_4/service/src/main/java/com/codegym/service/UserService.java
@@ -4,7 +4,11 @@ import com.codegym.dao.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 
 public interface UserService {
     Page<User> getAllUser(Pageable pageable);
+    List<User> getAllUser();
+    void save (User user);
 }
