06f4039a74066d75db1d930f2ab9cb1aa86fb824
Shinya9082 <anhtuandtu1993@gmail.com>
2020-03-03 21:05:09 +0700

    Update lại class Repository

--- module_4/dao/src/main/java/com/codegym/dao/repository/ProductCatalogueRepository.java
+++ module_4/dao/src/main/java/com/codegym/dao/repository/ProductCatalogueRepository.java
@@ -1,6 +1,5 @@
 package com.codegym.dao.repository;
 
-import com.codegym.dao.entity.Product;
 import com.codegym.dao.entity.ProductCatalogue;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
