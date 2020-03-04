32ac671bb13a3b5a4b133803e367731e6cc4ce11
chanhtv235 <chanhtv235@gmail.com>
2020-03-04 16:35:26 +0700

    chanh

--- module_4/service/src/main/java/com/codegym/service/ipml/RegisteredProductImpl.java
+++ module_4/service/src/main/java/com/codegym/service/ipml/RegisteredProductImpl.java
@@ -3,16 +3,19 @@ package com.codegym.service.ipml;
 import com.codegym.dao.entity.RegisteredProduct;
 import com.codegym.dao.repository.RegisteredProductRepository;
 import com.codegym.service.RegisteredProductService;
+import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public class RegisteredProductImpl implements RegisteredProductService {
     @Autowired
     RegisteredProductRepository registeredProductRepository;
     @Override
-    public List<RegisteredProduct> getAllRegisteredProduct() {
-        return (List<RegisteredProduct>) registeredProductRepository.findAll();
+    public List<RegisteredProduct> getAllRegisteredProductEndDay() {
+        return registeredProductRepository.getAllByProductEndDay();
     }
 }
