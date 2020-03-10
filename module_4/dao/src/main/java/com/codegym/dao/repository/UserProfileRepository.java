7699595df86dfa988a6301c86cbcdf9a837dcf14
Shinya9082 <anhtuandtu1993@gmail.com>
2020-03-10 16:55:14 +0700

    update

--- module_4/dao/src/main/java/com/codegym/dao/repository/UserProfileRepository.java
+++ module_4/dao/src/main/java/com/codegym/dao/repository/UserProfileRepository.java
@@ -1,14 +1,11 @@
 package com.codegym.dao.repository;
 
-import com.codegym.dao.entity.User;
 import com.codegym.dao.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile,Long> {
     UserProfile getUserProfileById(Long id);
     UserProfile getUserProfileByEmail(String email);
-    boolean findByEmail(String email);
 }
