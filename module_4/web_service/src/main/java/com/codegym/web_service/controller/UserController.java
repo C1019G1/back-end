ef3111d50c781030d0e9c143521c8538990a9706
thanhbui <thanhbui280197@gmail.com>
2020-03-13 17:03:42 +0700

    Thanh

--- module_4/web_service/src/main/java/com/codegym/web_service/controller/UserController.java
+++ module_4/web_service/src/main/java/com/codegym/web_service/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -52,8 +53,6 @@ public class UserController {
     UserLoginHistoryService userLoginHistoryService;
     @Autowired
     UserLockListService userLockListService;
-    @Autowired
-    UserDetailsService userDetailsService;
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ResponseEntity<?> saveUser(@RequestBody UserRegisterDTO userRegisterDTO) {
